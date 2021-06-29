9a2880b823faf21168aab1bb76790f5cf1e24f76
H3lix-Fire <ljh2004.03@outlook.com>
2021-06-29 05:31:40 -0700

    Basically just added and removed some stuff

--- download.php
+++ download.php
@@ -1,7 +1,3 @@
 <?php
-require_once 'protect.php';
-Protect\with('download.php', '1134');
+require_once('protect-this.php');
 ?>
-<body>
-  <p>Test</p>
-</body>
--- form.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'protect.php';
-Protect\with('form.php', '1134');
-?>
-<body>
-  <p>Test</p>
-</body>
